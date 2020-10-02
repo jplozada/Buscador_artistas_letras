34124fda6aed4b58dd76ec85d7c0d9c1fee348c8
jplozada <juxnpx@gmail.com>
2020-10-02 16:05:32 -0400

    saving Formulario form data on State

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Formulario = () => {
+
+    const [busqueda, guardarBusqueda] = useState ({
+        artista: '',
+        cancion: ''
+    })
+    const [error, guardarError] = useState(false);
+
+    const {artista, cancion} = busqueda;
+
+    // Funcion a cada input para leer su contenido
+
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Consultar las API's
+    const buscarInformacion = e => {
+        e.preventDefault();
+
+        // Validation
+        if(artista.trim() === '' || cancion.trim() === ''){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        // Pasar al componente principal
+        
+    }
+
     return (  
         <div className="bg-info">
             <div className="container">
                 <div className="row">
                     <form
+                        onSubmit={buscarInformacion}
                         className="col card text-white bg-transparent mb-5 pt-5 pb-2"
                     >
                         <fieldset>
@@ -22,6 +56,8 @@ const Formulario = () => {
                                             className="form-control"
                                             name="artista"
                                             placeholder="Nombre Artista"
+                                            onChange={actualizarState}
+                                            value={artista}
                                         />
                                     </div>
                                 </div>
@@ -33,6 +69,8 @@ const Formulario = () => {
                                             className="form-control"
                                             name="cancion"
                                             placeholder="Nombre Canción"
+                                            onChange={actualizarState}
+                                            value={cancion}
                                         />
                                     </div>
                                 </div>
